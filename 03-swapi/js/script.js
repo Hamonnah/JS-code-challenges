8404584ba6adf5ada9603aa1a6e60769135abb00
Monika Jozefina Bialka <hamonnah@Monikas-MacBook-Air.local>
2021-10-21 15:03:45 +0200

    actual

--- 03-swapi/js/script.js
+++ 03-swapi/js/script.js
@@ -2,35 +2,31 @@
 const btnFetch = document.getElementById('fetch');
 const btnSort = document.getElementById('sort');
     
-let planets =[];
-/* var planets;
-    doStuff = function(param) {
-      // param holds address, when you call it from `$.each()`
-      //param.sort();
-      console.log(param);
-    }; */
-
 const fetchData = () => {
-$(document).ready(function() {
-    $.getJSON('https://swapi.dev/api/planets/', function(data) {
-        let swapiData = '';
-        let obj = data.results;
 
-        $.each(obj, function(key, value) {
-            swapiData += `<li class="list-group-item">${value.name}</li>`;
-            planets += value.name;
-            //doStuff(planets);
-        });
-        $('#swapi-list').append(swapiData);
-    }); 
-});
-    btnFetch.classList.add('hide');
-    btnSort.classList.add('show');
+    fetch('https://swapi.dev/api/planets/')
+    .then(resp=>resp.json())
+    .then(data=>{
+        let allPlanets = data.results;
+        for(let planet of allPlanets) {
+
+            let item = document.createElement('li');
+            item.innerText = planet.name
+            item.classList.add('list-group-item')
+            document.getElementById('swapi-list').appendChild(item);   
+          
+        }    
+        planets = allPlanets;
+        btnFetch.classList.add('hide');
+        btnSort.classList.add('show');     
+    })
 };
 
-const sort = () => {
+console.log(planets);
+
+/* const sort = () => {
    
-    return [].slice.call(planets).sort(function (a,b) {
+    return [].slice.call(allPlanets).sort(function (a,b) {
        let nameA = a.toUpperCase();
        let nameB = b.toUpperCase();
        if (nameA < nameB) {
@@ -41,38 +37,13 @@ const sort = () => {
        }
        return 0;
    });  
-};
+};*/
 
-const sortList = () => {
-    sort(planets);
-    console.log(planets);
-};
+const sortList = (allPlanets) => {
 
+    console.log(allPlanets);
+};
+ 
 
 btnFetch.addEventListener('click', fetchData);
 btnSort.addEventListener('click', sortList);
-
-
-/* const btnFetch = document.getElementById('fetch');
-    fetch('https://swapi.dev/api/planets/')
-        .then(response => response.json())
-        .then(data => {
-
-            
-            let planetName = document.createElement('p');
-
-            /* planetName.innerText = `Planet name: ${data}`; */
-            //planetName.innerText = `Planet name: ${data.results}`;
-/*             planetName.innerText = JSON.stringify(data.results, ['name'], 2);
-            
-           
-            document.body.appendChild(planetName);
-            console.log(data.results);
-            console.log(data.results.length);
-            console.log(Object.values(data.results));
-
-        })
-}
-
-btnFetch.addEventListener('click', fetchData); */
- 
